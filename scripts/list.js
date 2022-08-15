dcd3a3fe012a1cff29ab9d022ae0a31e9c7d2172
InvincibleSun <103495967+InvincibleSun@users.noreply.github.com>
2022-08-15 17:30:09 +0400

    add a local storage

--- scripts/list.js
+++ scripts/list.js
@@ -1,10 +1,11 @@
+// Take data from LocalStorage, if it is empty take a new empty array
 let storage = localStorage.getItem("storage") || [];
 
 if (typeof storage === "string") {
-  storage = JSON.parse(storage);
+  storage = JSON.parse(storage); // string => object
 }
 
-console.log(storage);
+// console.log(storage);
 // show old list items
 
 const listExample = document.getElementById("list-example");
@@ -13,19 +14,42 @@ const mainSection = document.getElementsByClassName("main__section")[0];
 function addList() {
   const listName = prompt("Put the name of your new list").trim();
 
+  // Check correct form of List Name
   if (!listName) {
     alert("Put the correct name");
+    addList();
     return;
   }
 
+  // Copy List from example in the bottom of HTML
   const newList = listExample.cloneNode(true);
   newList.removeAttribute("id");
 
-  const titleNode = newList.querySelector(".main__section__list__name b");
-  titleNode.innerHTML = listName;
+  // Push List Name in the List
+  const nodeTitle = newList.querySelector(".main__list-name b");
+  nodeTitle.innerHTML = listName;
 
+  // Add List in the Document
   mainSection.appendChild(newList);
+
+  // Add List Name in Local Storage
   storage.push({ listName });
 
+  // Create storage data as a string
   localStorage.setItem("storage", JSON.stringify(storage));
+}
+
+document.addEventListener("DOMContentLoaded", pinList); // Create Event - if Reload of page => do a function
+
+function pinList() {
+  // Take every List from storage (loop) and append in document(after reload)
+  storage.forEach(({ listName }) => {
+    const newList = listExample.cloneNode(true);
+    newList.removeAttribute("id");
+
+    const nodeTitle = newList.querySelector(".main__list-name b");
+    nodeTitle.innerHTML = listName;
+
+    mainSection.appendChild(newList);
+  });
 }
