cf6d8fe188c8aa98e71087f84e9d1ee45874cc20
Artem Alchangyan <alchangyan@mail.ru>
2022-08-18 20:57:03 +0400

    REMOVE LIST

--- scripts/list.js
+++ scripts/list.js
@@ -1,72 +1,63 @@
-// Take data from LocalStorage + create my personal data storage in var, if it is empty take a new empty array
-let storage = localStorage.getItem("storage") || [];
-const startIndex = 0;
+// INITIALIZATION
+document.addEventListener("DOMContentLoaded", () => {
+  storage.forEach(drawList);
+});
+
+let storage = localStorage.getItem("store") || [];
 
 if (typeof storage === "string") {
-  storage = JSON.parse(storage); // string => object
+  storage = JSON.parse(storage);
 }
 
-// show old list items
-
+// CONSTANTS
 const listExample = document.getElementById("list-example");
 const mainSection = document.getElementsByClassName("main__section")[0];
+const startIndex = 0;
 
+// ACTIONS
+// Creation of new list
 function addList() {
   const listName = prompt("Put the name of your new list").trim();
 
-  // Check correct form of List Name
   if (!listName) {
     alert("Put the correct name");
     addList();
     return;
   }
 
-  // define new id
-  const id = storage.length ? storage[storage.length - 1].id + 1 : startIndex; // ???
+  const nextId = storage.length
+    ? storage[storage.length - 1].id + 1
+    : startIndex;
 
-  const newList = { listName, id };
-  // Copy List from example in the bottom of HTML
+  const newList = { listName, id: nextId };
   drawList(newList);
 
-  // Add List Name in Local Storage
   storage.push(newList);
 
-  // Create storage data as a string
-  localStorage.setItem("storage", JSON.stringify(storage));
+  localStorage.setItem("store", JSON.stringify(storage));
 }
 
-document.addEventListener("DOMContentLoaded", pinList); // Create Event - if Reload of page => do a function
-
-function pinList() {
-  // Take every List from storage (loop) and append in document(after reload)
-  storage.forEach(drawList);
-}
-// const closeBtn = document.querySelectorAll(".main__list-btn");
-
+// Add new list to body
 function drawList(listItem) {
   const { listName, id } = listItem;
   const newList = listExample.cloneNode(true);
   newList.removeAttribute("id");
-  newList.setAttribute("data-id", id); //???
-  newList.querySelector(".main__list-btn").setAttribute("data-id", id);
-
+  newList.setAttribute("data-id", id);
   const nodeTitle = newList.querySelector(".main__list-name b");
   nodeTitle.innerHTML = listName;
-
   mainSection.appendChild(newList);
 }
 
-// REMOVE LIST
+// List removal
 function removeList(e) {
-  let { id } = e.currentTarget.dataset;
+  const parent = e.target.closest(".main__list");
+  let { id } = parent.dataset;
   id = Number(id);
-  console.log(id);
+  parent.remove();
+  const toDeleteIndex = storage.findIndex((elem) => elem.id === id);
 
-  // delete from UI
-  newList.classList.remove(".main__list");
-
-  // delete from store
-
-  // delete from local store
-  // localStorage.setItem("storage", JSON.stringify(storage));
+  if (toDeleteIndex !== -1) {
+    storage.splice(toDeleteIndex, 1);
+    localStorage.setItem("store", JSON.stringify(storage));
+  }
 }
